54c763863c80476980ec33cea5eb4b45d1a4c8ca
Vondrs <107077915+Vondrs7@users.noreply.github.com>
2022-11-07 19:54:04 +0100

    Added method for json data flattening

--- data_parsing.py
+++ data_parsing.py
@@ -10,8 +10,27 @@ class ReadData:
             data = json.loads(x)
             return data
 
+    # "flattening" json data
+    def data_flattening(self, x):
+        flattened_data = {}
+        def flattening( y, name="" ):
+            if type(y) is dict:
+                for z in y:
+                    flattening(y[z], name + z + "_")        
+            elif type(y) is list:
+                n = 0
+                for z in y:
+                    flattening(z, name + str(n) + "_")
+                    n += 1
+            else:
+                flattened_data[name[:-1]] = x
+        flattening(x)
+        return flattened_data        
+
 
 if __name__ == '__main__':
     
     read_data = ReadData()
     data = read_data.read_json()
+    data = read_data.data_flattening(data)
+
